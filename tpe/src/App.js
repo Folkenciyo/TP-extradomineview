8842faf7a032bd0394e7b5f817e933c38c712ca3
Folkenciyo <86403857+Folkenciyo@users.noreply.github.com>
2022-05-31 10:56:51 +0200

    fin

---
+++ tpe/src/App.js
@@ -0,0 +1,14 @@
+import PNG from './PNG-Me-alimentas.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <a href="/">
+        <img src={PNG} alt="img"/>
+      </a>
+    </div>
+  );
+}
+
+export default App;
